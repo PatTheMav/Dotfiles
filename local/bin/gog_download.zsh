783583e5517ff534b4d514ca34c82dbb8ae050e6
PatTheMav <PatTheMav@users.noreply.github.com>
2025-01-15 17:34:50 +0100

    Update GOG download script to include md5 hash checks

--- local/bin/gog_download.zsh
+++ local/bin/gog_download.zsh
@@ -1,6 +1,5 @@
 #!/usr/bin/env zsh
 
-
 if (( ! # || # < 2 )) {
     print -u2 -PR "%F{1}Usage: %B${0}%b <URL LIST> <COOKIE_FILE>%f"
     exit 2
@@ -16,6 +15,30 @@ local -a files=($(<${url_list}))
 
 for file (${files}) {
     local -a locations=($(curl -s -I -L ${file} -H ${gog_cookie} | tr -d '\r' | sed -En 's/^[lL]ocation: (.*)/\1/p'))
-    local decoded=$(printf "%b" "${${${locations[-1]}:t}//\%/\\x}")
-    axel --no-clobber --num-connections=5 -H ${gog_cookie} --output="${decoded}" ${file}
+    local file_url=${locations[-1]}
+    local decoded=$(printf "%b" "${${file_url:t}//\%/\\x}")
+
+    local md5_hash=""
+    local md5_hash_actual=""
+
+    if [[ ${decoded:e} == (exe|bin|pkg) ]] {
+        md5_hash=$(curl -s "${file_url}.xml" | sed -E -n -e 's/^<file.*md5="([^"]+)".*>/\1/p' | tr -d '\r\n')
+    }
+
+    axel --no-clobber --num-connections=5 -H ${gog_cookie} --output="${decoded}" ${file_url}
+
+    if [[ ${md5_hash} ]] {
+        if (( ${+commands[pv]} )) {
+            md5_hash_actual=$(pv ${decoded} | md5sum -b)
+        } else {
+            md5_hash_actual=$(md5sum -b ${decoded})
+        }
+
+        md5_hash_actual="${${(s: :)md5_hash_actual}[1]}"
+
+        if [[ ${md5_hash_actual} != ${md5_hash} ]] {
+            print -u2 -PR "%F{1}MD5 hash mismatch of downloaded file!"
+            exit 2
+        }
+    }
 }
